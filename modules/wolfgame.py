aafbf2331d73876c8342cb068f895441ae974058
Epithumia <lopez@airelle.info>
2014-01-09 10:41:11 +0100

    Update copyright notices in some files.

--- modules/wolfgame.py
+++ modules/wolfgame.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2011, Jimmy Cao
+# Copyright (c) 2011, Jimmy Cao, (c) 2014, Rafael Lopez
 # All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
--- pyIRCbot.py
+++ pyIRCbot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.2
 
-# Copyright (c) 2011 Jimmy Cao
+# Copyright (c) 2011 Jimmy Cao, (c) 2014 Rafael Lopez
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
